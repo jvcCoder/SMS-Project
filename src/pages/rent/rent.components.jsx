d44c416d620a2fd19f02d9265bc0719558f1e594
jvcCoder <you@example.com>
2021-08-10 19:01:49 +0100

    Get google maps from Firebase and display it in Rent Page

--- src/pages/rent/rent.components.jsx
+++ src/pages/rent/rent.components.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import RENT_DATA from "./rent.data.js";
 import RentPreview from "../../components/rent-preview/rent-preview.component";
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 class RentPage extends React.Component {
   constructor(props) {
@@ -12,15 +13,34 @@ class RentPage extends React.Component {
   }
 
   render() {
+    const mapStyles = {
+      width: '10vw',
+      height: '10vh',
+    }; 
     const { collections } = this.state;
     return (
       <div className='rent-page'>
         {collections.map(({ id, ...otherCollectionProps }) => (
           <RentPreview key={id} {...otherCollectionProps} />
         ))}
+        <div className='maps'>
+        <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={
+            {
+              lat: 53.350145,
+              lgn: -6.266157,
+            }
+          }
+        />
+        </div>
       </div>
     );
   }
 }
 
-export default RentPage;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCCqUaqUaSpRtLShd2Tyi5PuphF94YhzmQ'
+})(RentPage);
